16023390d908cf5b10b808df55ccab123074a509
florchiexco <florchiexco@gmail.com>
2020-05-05 15:04:40 -0300

    sql file of the der done

---
+++ Schema/schema.sql
@@ -0,0 +1,98 @@
+CREATE DATABASE utn_phones;
+USE utn_phones;
+
+CREATE TABLE line_types(
+	id INT, 
+    type VARCHAR(30),
+    CONSTRAINT pk_id_line_type PRIMARY KEY (id)
+);
+
+CREATE TABLE phone_lines(
+	id INT,
+    number VARCHAR(9) UNIQUE,
+    line_type INT,
+    CONSTRAINT pk_id_phone_line PRIMARY KEY (id),
+    CONSTRAINT fk_line_type FOREIGN KEY (line_type) REFERENCES line_types (id)
+);
+
+CREATE TABLE provinces(
+	id INT,
+    name VARCHAR(30) UNIQUE,
+    CONSTRAINT pk_id_province PRIMARY KEY (id)
+);
+
+CREATE TABLE cities(
+	id INT,
+    prefix INT(3) UNIQUE,
+    name VARCHAR(30) UNIQUE,
+    province INT,
+    CONSTRAINT pk_id_city PRIMARY KEY (id),
+    CONSTRAINT fk_id_province FOREIGN KEY (province) REFERENCES provinces (id)
+);
+
+CREATE TABLE user_types(
+	id INT,
+    type VARCHAR(30),
+    CONSTRAINT pk_id_user_type PRIMARY KEY (id)
+);
+
+CREATE TABLE users(
+	id INT,
+    dni VARCHAR(9) UNIQUE,
+    username VARCHAR(30) UNIQUE,
+    password VARCHAR(30),
+    name VARCHAR(30),
+    surname VARCHAR(30),
+    city INT,
+    province INT,
+    user_type INT,
+    CONSTRAINT pk_id_user PRIMARY KEY (id),
+    CONSTRAINT fk_id_city FOREIGN KEY (city) REFERENCES cities (id),
+    CONSTRAINT fk_id_province2 FOREIGN KEY (province) REFERENCES provinces (id),
+    CONSTRAINT fk_user_type FOREIGN KEY (user_type) REFERENCES user_types (id)
+);
+
+CREATE TABLE bills(
+	id INT,
+    quantity_of_calls INT,
+    cost_price FLOAT,
+    total_price FLOAT,
+    date DATE, 
+    expiring_date DATE,
+    client INT,
+    phone_line INT,
+    CONSTRAINT pk_id_bill PRIMARY KEY (id),
+    CONSTRAINT fk_id_client FOREIGN KEY (client) REFERENCES users (id),
+    CONSTRAINT fk_id_phone_line FOREIGN KEY (phone_line) REFERENCES phone_lines(id)
+);
+
+CREATE TABLE tariffs(
+	origin_city INT,
+    destiny_city INT,
+    cost_price FLOAT,
+    price_per_minute FLOAT,
+    CONSTRAINT fk_origin_city FOREIGN KEY (origin_city) REFERENCES cities (id),
+    CONSTRAINT fk_destiny_city FOREIGN KEY (destiny_city) REFERENCES cities (id)
+);
+
+CREATE TABLE calls(
+	id INT,
+    price_per_minute FLOAT,
+    duration TIME,
+    date DATETIME,
+    cost_price FLOAT,
+    total_price FLOAT,
+    origin_phone_line VARCHAR(9),
+    destiny_phone_line VARCHAR(9),
+    id_origin_line INT,
+    id_destiny_line INT,
+    origin_city INT,
+    destiny_city INT,
+    bill INT,
+    CONSTRAINT pk_id_call PRIMARY KEY (id),
+    CONSTRAINT fk_id_origin_line FOREIGN KEY (id_origin_line) REFERENCES phone_lines (id),
+    CONSTRAINT fk_id_destiny_line FOREIGN KEY (id_destiny_line) REFERENCES phone_lines (id),
+    CONSTRAINT fk_origin_city2 FOREIGN KEY (origin_city) REFERENCES cities (id),
+    CONSTRAINT fk_destiny_city2 FOREIGN KEY (destiny_city) REFERENCES cities (id),
+    CONSTRAINT fk_id_bill FOREIGN KEY (bill) REFERENCES bills (id)
+);
