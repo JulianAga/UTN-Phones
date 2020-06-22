9e5c71ca9322bda765dd15708dea56ddeb5191bc
JulianAga <48849848+JulianAga@users.noreply.github.com>
2020-06-22 03:33:49 -0300

    swagger 2 & rest template configured

---
+++ src/main/java/com/utn/phones/configuration/RestTemplateConfiguration.java
@@ -0,0 +1,14 @@
+package com.utn.phones.configuration;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+public class RestTemplateConfiguration {
+
+  @Bean("restTemplate")
+  RestTemplate createRestTemplate(){
+    return new RestTemplate();
+  }
+
+}
